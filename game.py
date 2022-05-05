5554d9fe9fc7c20cac1ed73e762287331c60ace1
LavenLowe <97907128+LavenLowe@users.noreply.github.com>
2022-05-05 11:24:15 +0300

    Develop3.0

--- game.py
+++ game.py
@@ -1,5 +1,12 @@
-import models
-import settings
-import exceptions
-fullname = input("Fullname: ")
+from models import Player, Enemy
+
+if __name__ == '__main__':
+    enm = Enemy()
+    pl = Player()
+    while True:
+        pl.allowed_attacks()
+        enm.select_attack()
+
+
+
 
--- models.py
+++ models.py
@@ -1,49 +1,48 @@
-from random import randint
-
+from random import randint, randrange
+from exceptions import GameOver, EnemyDown
+import time
 class Player:
-	score = 0
-	def allowed_attacks(self, enemy_lives):
+	
+	def __init__(self):
+		self.enemy_lives = randint(1, 3)
+	def allowed_attacks(self):
 		player_attack = int(input('Enter you attack type: '))
 		if player_attack == 0:
 			print('You have chosen to attack the enemy')
-			attack = enemy_lives - 1
-			print('Enemy lifebar: ', enemy_lives)
-			if enemy_lives == 1 or enemy_lives == 2:
+			self.enemy_lives -= 1
+			print('Enemy lifebar: ', self.enemy_lives)
+			if self.enemy_lives == 1 or self.enemy_lives == 2:
 				pass
 
-			elif enemy_lives <= 0:
+			elif self.enemy_lives <= 0:
 				print('Enemy dead')
+				
 				
 		elif player_attack == 1:
 			print('You chose to defend')
-
-
+		else:
+			print('Wrong key!!!')
+			exit()
 
 class Enemy:
-	def __init__(self, players_lives, type_attack):
-		self.players_lives = players_lives
-		self.type_attack = type_attack		
-		super().__init__(players_lives, type_attack)
-
+	
+	def __init__(self):
+		self.player_lives = 10
 
 	def select_attack(self):
+		
+		type_attack = randint(0, 1)
 		if type_attack == 0:
 			print('Enemy attack you')
-			attack = players_lives - 1
-			print('Player lifebar: ', attack)
-
-			if players_lives == 0:
-				#class GameOver
-				print('GameOver')
-			else:
-				pass
+			self.player_lives -= 1
+			print('Player lifebar: ', self.player_lives)
 		elif type_attack == 1:
 			print('Enemy defend')
+		if self.player_lives <= 0:
+			print('Yor scores: ', 10)
+			time.sleep(2)
+			print('See you next time!')
+			time.sleep(2)
+			raise GameOver
 
-name = input("Fullname: ")
-if __name__ == '__main__':
-	players_lives = 10
-	while players_lives >= 0:
-		type_attack = randint(0, 1)
-		enm = Enemy(10, type_attack)
-		pl = Player(randint(0,2))
+
